23713668b10355b65340e9477d3a348a50a9d58a
tomato <tomato414941@gmail.com>
2025-01-25 02:36:49 +0900

    fix: Update getRequestConfig to properly handle locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,16 @@
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
 import {locales} from './config';
 
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`../messages/${locale}.json`)).default,
-  locale: locale,
-  timeZone: 'Asia/Tokyo'
-}));
+export default getRequestConfig(async ({locale}) => {
+  // 
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default,
+    timeZone: 'Asia/Tokyo',
+    now: new Date(),
+    // 
+    locale: locale
+  };
+});
