443c9dd2f4b6d98ff07e5e1fd93e09a06733366f
tomato <tomato414941@gmail.com>
2025-01-25 02:42:05 +0900

    fix: Further simplify layout structure to resolve hydration issues

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -55,7 +55,9 @@ export default async function RootLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           <AuthProvider>
             <div className="min-h-full">
-              <LanguageSwitcher />
+              <div className="fixed top-4 right-4 z-50">
+                <LanguageSwitcher />
+              </div>
               {children}
             </div>
           </AuthProvider>
